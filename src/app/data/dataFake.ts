591e1d06692a0df1f7e93e968146fa21e8f146cc
Bruno Pires <brunopires.ctto@gmail.com>
2023-10-11 12:51:19 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Em parceria com a Marvel Games, a Electronics Arts (EA) anunciou hoje, dia 20/09, que um novo jogo do Homem de Ferro está em desenvolvimento. O jogo de ação e aventura será em terceira pessoa e single player, e atualmente está em produção pelo Motive Studio.",
+    "photoCover":"https://tudodenerd.com.br/wp-content/uploads/2020/09/Tom-Cruise-como-novo-Homem-de-Ferro-e1601073731525.png"
+  },
+  {
+    "id":"2",
+    "title": "Novo filme do Homem-Aranha chega às telonas",
+    "description": "Miles é um adolescente afro-latino-americano, que vive em Nova York, e que assume o manto do Homem-Aranha após a morte de Peter Parker em sua dimensão. A história de Miles Morales começa em Ultimate Comics: Spider-Man #1, em setembro de 2011",
+    "photoCover":"https://www.leiagora.com.br/imgsite/entrete/amp-Homem-aranha-Atraves-do-aranhaverso-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Marvel anuncia dois novos filmes dos Vingadores",
+    "description": "A Marvel Studios revelou no sábado, 23, durante a San Diego Comic-Con o calendário completo de lançamentos de filmes e séries do seu Universo Cinematográfico. O produtor executivo do estúdio, Kevin Feige, anunciou o calendário completo das Fase 5 e 6 do MCU. A Marvel detalhou que os filmes das fase 4, 5 e 6 fazem parte da Saga do Multiverso.",
+    "photoCover":"https://classic.exame.com/wp-content/uploads/2022/07/marvel.jpg?quality=70&strip=info&w=1024"
+  },
+  {
+    "id":"4",
+    "title":'Guardiões da Galáxia: Segundo James Gunn, Peter Quill não é...',
+    "description": 'Para Gunn, Rocket Raccoon (Bradley Cooper) é o herói da série, como explicou em entrevista à Entertainment Weekly: "Para mim, Rocket sempre foi o protagonista anônimo dos filmes Guardiões. Desde o início, os filmes foram enraizados em quem ele é como personagem. Acho que ele incorpora muitas características de todos os Guardiões. Todos tiveram seus traumas, que os uniram. Só que ele é mais extremo."Em Rocket, Gunn vê tudo o que caracteriza os Guardiões em sua forma mais pura.',
+    "photoCover":"https://itajaishopping.com.br/wp-content/uploads/2023/05/image-1024x683.png"
+  }
+]
